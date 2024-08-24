af7ca4e17525f687b43389edb7cab62fb8873664
Camilo Parra <116674187+FullstackBK@users.noreply.github.com>
2024-08-24 09:47:28 -0500

    configuracion para deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false
+  //  output: 'export',
+   swcMinify: false,
+  // reactStrictMode: false
 }
 
 module.exports = nextConfig
