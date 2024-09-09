0225c2a5bae778471a071ff6666df6e3e579711b
PC-HP <116674187+FullstackBK@users.noreply.github.com>
2024-09-09 14:49:09 -0500

    / output: 'export' desactivado

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-      output: 'export'
-  //  swcMinify: false,
+      // output: 'export'
+   swcMinify: false,
   // reactStrictMode: false
 }
 
